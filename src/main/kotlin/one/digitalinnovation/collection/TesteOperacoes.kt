f821eb87642dc28c6c2b47d4b909f674cd2345cf
Leo211990 <leonnam.souza@gmail.com>
2022-08-21 19:49:21 -0300

    Commit Teste Operações

---
+++ src/main/kotlin/one/digitalinnovation/collection/TesteOperacoes.kt
@@ -0,0 +1,20 @@
+package one.digitalinnovation.collection
+
+fun main() {
+    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
+    for(salario in salarios){
+        println(salario)
+    }
+
+    println("--------------------------------")
+    println("Maior salário: ${salarios.maxOrNull()}")
+    println("Menor salário: ${salarios.minOrNull()}")
+    println("Média dos salários: ${salarios.average()}")
+
+    val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
+
+    println("--------------------------------")
+    salariosMaiorQue2500.forEach{(println(it))}
+
+
+}
