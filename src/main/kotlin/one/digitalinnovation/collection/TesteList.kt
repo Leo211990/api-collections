201d294ee790ee8253d810a01cab98ba5ec97b0f
Leo211990 <leonnam.souza@gmail.com>
2022-08-22 09:31:52 -0300

    Commit Teste List

--- src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collection/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collection
 
 fun main() {
-    val joao = Funcionario("Joao", salario =  1000.0)
-    val pedro = Funcionario("Pedro", salario =  2000.0)
-    val maria = Funcionario("Maria", salario =  4000.0)
+    val joao = Funcionario("Joao", salario =  2000.0, tipoContratacao = "CLT")
+    val pedro = Funcionario("Pedro", salario =  1500.0, tipoContratacao = "PJ")
+    val maria = Funcionario("Maria", salario =  4000.0, tipoContratacao = "CLT")
 
     val funcionarios = listOf(joao, pedro, maria)
 
@@ -12,11 +12,24 @@ fun main() {
     println("-------------------------------------------")
     println(funcionarios.find { it.nome == "Maria" })
 
+    println("-------------------------------------------")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) }
+
+    println("-------------------------------------------")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach { println(it) }
+
+
+
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String,
 ){
     override fun toString(): String =
         """
